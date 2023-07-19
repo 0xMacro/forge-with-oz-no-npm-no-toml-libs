e1c9384feeded858dde9654a494b1f4d17345646
Gilbert <gilbertbgarza@gmail.com>
2023-07-19 12:52:45 -0500

    First commit

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/draft-IERC20PermitUpgradeable.sol";
+
 contract Counter {
+    IERC20PermitUpgradeable addr;
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
